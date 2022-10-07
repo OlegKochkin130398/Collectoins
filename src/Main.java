4de98930cb8083868b57229880e363812690e388
oleg- <o.boys@yandex.ru>
2022-10-07 15:17:37 +0300

    Война и мир

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    final static String pattern = "страдание";
+    final static String regex = "^[Сс]трада(.*)";
+
+    public static void main(String[] args) {
+        File file = new File("warofpeace.txt");
+        Parser parser = new Parser();
+
+        ArrayList<String> lines = parser.parse(file);
+        ArrayList<String> suffwords = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile(regex);
+
+        for(String line : lines){
+            if(pattern.matcher(line).find()){
+                suffwords.add(line);
+            }
+        }
+
+        System.out.println(suffwords.size());
+
+
+    }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Parser {
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Parser {
+
+    final static String pattern = "";
+
+    ArrayList<String> parse(File file){
+        ArrayList<String> lines = new ArrayList<>();
+        ArrayList<String> words = new ArrayList<>();
+        try (Scanner scanner = new Scanner(file).useDelimiter("[^а-яА-я]+")){
+
+            String line = "";
+
+            while(scanner.hasNextLine()){
+                lines.add(line);
+                line = scanner.nextLine();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        for(String line : lines){
+            words.addAll(Arrays.asList(line.split("[^а-яА-я]+")));
+
+        }
+        return words;
+    }
 }
